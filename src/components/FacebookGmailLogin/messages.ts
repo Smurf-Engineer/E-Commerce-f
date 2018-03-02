f7a3acc385802acf4efc17671f805abbac05290a
Carlos Cazarez <carlos@tailrecursive.co>
2018-03-02 12:47:59 -0700

    fixed styles in facebook login button, added messages.ts, deleted unused css classes in App.css

---
+++ src/components/FacebookGmailLogin/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  googleLoginLabel: {
+    id: 'components.menuBar.googleLoginLabel',
+    defaultMessage: 'LOG IN WITH GOOGLE'
+  },
+  facebookLoginLabel: {
+    id: 'components.menuBar.facebookLoginLabel',
+    defaultMessage: 'LOG IN WITH FACEBOOK'
+  }
+})
