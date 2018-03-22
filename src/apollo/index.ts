e17b463a134534333b9eb65e590630db1fe20a3b
Gustavo Medina <gustavomedina@MacBook-Pro-de-Gustavo.local>
2018-03-21 17:49:52 -0700

    uri

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -5,7 +5,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'node-fetch'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4040/api/graphql',
+  uri: 'https://dev.api.jakroo.shipnote.io/api/graphql',
   fetch: fetch as any
 })
 
