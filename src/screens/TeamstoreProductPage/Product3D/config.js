f6d581cb7e31e2746eebb41fcb1d4bd2273556c1
David Gonzalez <david@tailrecursive.co>
2018-05-25 10:43:24 -0600

    Fix 3D model in product page.

--- src/screens/TeamstoreProductPage/Product3D/config.js
+++ src/screens/TeamstoreProductPage/Product3D/config.js
@@ -3,19 +3,13 @@
  */
 
 export const jerseyTextures = {
-  backPocket: './models/Tour5/branding.png',
-  color1:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/PNGTEST/colorblock_1.svg.png',
-  color2:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/PNGTEST/colorblock_2.svg.png',
-  color3:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/PNGTEST/colorblock_3.svg.png',
-  color4:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/PNGTEST/colorblock_4.svg.png',
-  color5:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/PNGTEST/colorblock_5.svg.png',
-  flatlock: './models/Tour5/flatlock.png',
-  label: './Red-J.jpg',
-  bumpMap:
-    'https://storage.googleapis.com/jakroo-storage/models/Tour/Tour_v2-BumpMap.jpg'
+  obj: 'https://storage.googleapis.com/jakroo-storage/models/Tour/Tour_v2.obj',
+  mtl: 'https://storage.googleapis.com/jakroo-storage/models/Tour/Tour_v2.mtl',
+  images: {
+    texture:
+      'https://storage.googleapis.com/jakroo-storage/models/Tour/C01-D01.svg',
+    flatlock: './models/images/flatlock.png',
+    bumpMap:
+      'https://storage.googleapis.com/jakroo-storage/models/Tour/Tour_v2-BumpMap.jpg'
+  }
 }
