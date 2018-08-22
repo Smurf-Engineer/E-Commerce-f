cd718cbec8cb4b902a689493de9794bcc9110eae
Jorge Treviño <jorge@tailrecursive.co>
2018-08-22 15:05:09 -0600

    Modified LogoutModal to use GRAY_DARK color constant

--- src/components/LogoutModal/styledComponents.tsx
+++ src/components/LogoutModal/styledComponents.tsx
@@ -2,9 +2,10 @@
  * Styled Components - Created by jorge on 22/08/18.
  */
 import styled from 'styled-components'
+import { GRAY_DARK } from '../../theme/colors'
 
 export const DeleteConfirmMessage = styled.div`
-  color: #5f6062;
+  color: ${GRAY_DARK};
   font-size: 16px;
   letter-spacing: 0.2px;
   line-height: 22px;
