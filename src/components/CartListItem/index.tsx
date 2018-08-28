d75a12c62d7d4fbcc91e6b5376579c4f8cd8e757
Miguel Canobbio <miguel@tailrecursive.co>
2018-08-28 15:32:58 -0600

    delete logs

--- src/components/CartListItem/index.tsx
+++ src/components/CartListItem/index.tsx
@@ -219,9 +219,6 @@ class CartListItem extends React.Component<Props, {}> {
     const { designId, designName, designImage, designCode } = cartItem
 
     const retailProduct = !designId
-    console.log('---------------------------')
-    console.log(retailProduct)
-    console.log('---------------------------')
 
     const quantities = cartItem.itemDetails.map((itemDetail, ind) => {
       return itemDetail.quantity
