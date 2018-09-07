2b38455126f8d940e245fae4c15ca0d44bd13976
Jorge Treviño <jorge@tailrecursive.co>
2018-09-07 15:19:24 -0600

    Fixed issue with DragText overlaping with 3DModel in DesignCenter

--- src/components/DesignCenterCustomize/Render3D/styledComponents.js
+++ src/components/DesignCenterCustomize/Render3D/styledComponents.js
@@ -71,9 +71,17 @@ export const DragText = styled.div`
   font-size: 14px;
   line-height: 23px;
   position: absolute;
-  bottom: 32%;
+  bottom: 5%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  @media (min-height: 800px) and (max-height: 950px) {
+    bottom: 14%;
+  }
+
+  @media (min-height: 951px) {
+    bottom: 30%;
+  }
 `
 
 export const ModelType = styled.div`
