590f03e3e1b7c54c96178075e2c0e4b26b268da7
Carlos Cazarez <carlos@tailrecursive.co>
2018-09-28 16:41:14 -0600

    hide dropdown menu after clickng on thumbnail

--- src/config/index.ts
+++ src/config/index.ts
@@ -24,7 +24,7 @@ interface ConfigType {
 const config: ConfigType = {
   googleId: process.env.GOOGLE_ID,
   facebookId: process.env.FACEBOOK_ID,
-  graphqlUriBase: process.env.GRAPHQL_URI_BASE,
+  graphqlUriBase: 'http://localhost:4040/api/', // process.env.GRAPHQL_URI_BASE,
   baseUrl: process.env.BASE_URL,
   storageUrl: process.env.STORAGE_URL,
   pkStripeUS: process.env.PK_STRIPE_US,
