bc31ac7ba22d2bc505614e14311b003bf185029f
David Gonzalez <david@tailrecursive.co>
2018-10-18 15:56:51 -0600

    Added for loop test.

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -2,15 +2,23 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import routes from '../../config/routes'
 import LanguageProvider from '../../screens/LanguageProvider'
-// TODO: Test
+// TODO: TEST
 // import LogRocket from 'logrocket'
+// import config from '../../config'
 import { translationMessages } from '../../locale'
 import './theme.ant'
 import './App.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-// LogRocket.init('thy42y/jakroo')
+let testVar
+// LogRocket.init(config.logRocketKey || '')
+for (let index = 0; index < Number.MAX_SAFE_INTEGER; index++) {
+  // TODO: TEST
+  testVar = index * 2
+}
+
+console.log(testVar)
 
 const App = (props: any) => (
   <LanguageProvider messages={translationMessages}>
