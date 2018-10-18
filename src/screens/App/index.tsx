84c9eeecb41cdd013f622a77cc2538d353622fad
David Gonzalez <david@tailrecursive.co>
2018-10-18 16:40:29 -0600

    Removed timeout.

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -12,12 +12,6 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 // LogRocket.init(config.logRocketKey || '')
-setTimeout(() => {
-  console.log('---------------------------')
-  console.log('Hi!!!')
-  console.log('---------------------------')
-  // tslint:disable-next-line:align
-}, 1000)
 
 const App = (props: any) => (
   <LanguageProvider messages={translationMessages}>
