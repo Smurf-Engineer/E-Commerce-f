bdffbced955660f0c8a05bab86306030146113e4
Fernanda Aispuro <fernanda@tailrecursive.co>
2018-11-23 13:21:55 -0800

    Setting error when country does not belong to EU subsidiary

--- src/screens/Checkout/constants.ts
+++ src/screens/Checkout/constants.ts
@@ -20,6 +20,7 @@ export const VALID_BILLING_FORM = `${namespace}/VALID_BILLING_FORM`
 export const SAME_BILLING_AND_SHIPPING_CHECKED = `${namespace}/SAME_BILLING_AND_SHIPPING_CHECKED`
 export const SAME_BILLING_AND_SHIPPING_UNCHECKED = `${namespace}/SAME_BILLING_AND_SHIPPING_UNCHECKED`
 export const SET_STRIPE_ERROR = `${namespace}/SET_STRIPE_ERROR`
+export const SET_IBAN_ERROR = `${namespace}/SET_IBAN_ERROR`
 export const SET_LOADING_BILLING = `${namespace}/SET_LOADING_BILLING`
 export const SET_STRIPE_TOKEN = `${namespace}/SET_STRIPE_TOKEN`
 export const SET_STRIPE_CARD_DATA = `${namespace}/SET_STRIPE_CARD_DATA`
@@ -64,3 +65,11 @@ export const COUNTRY_CODE_CH = 'ch'
 export const COUNTRY_CODE_ES = 'es'
 export const COUNTRY_CODE_IT = 'it'
 export const COUNTRY_CODE_FR = 'fr'
+export const EU_SUBSIDIARY_COUNTRIES = [
+  COUNTRY_CODE_AT,
+  COUNTRY_CODE_DE,
+  COUNTRY_CODE_CH,
+  COUNTRY_CODE_ES,
+  COUNTRY_CODE_IT,
+  COUNTRY_CODE_FR
+]
