898192294f4eae0f9254ed20a1c2c65fc6f6e347
Eduardo Quintero <eduardoquintero@Eduardo.local>
2018-11-29 17:43:58 -0700

    Tag manager id according to env

--- src/helpers/Html.tsx
+++ src/helpers/Html.tsx
@@ -10,10 +10,9 @@ const Html = ({ content, state, reduxState }: Props) => (
   <body>
     <noscript>
       <iframe
-        src={
-          'https://www.googletagmanager.com/ns.html?id=GTM-' +
+        src={`https://www.googletagmanager.com/ns.html?id=GTM-${
           config.googleTagManagerId
-        }
+        }`}
         height="0"
         width="0"
         style={{ display: 'none', visibility: 'hidden' }}
