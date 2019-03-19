2f54f9e81d7a78814d2a40e5fc9768b56dcbbbaf
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2019-03-19 14:59:52 -0700

    Share model fonts

--- src/screens/Designs/index.tsx
+++ src/screens/Designs/index.tsx
@@ -4,13 +4,16 @@
 import * as React from 'react'
 import { injectIntl, InjectedIntl } from 'react-intl'
 import { RouteComponentProps } from 'react-router-dom'
+import get from 'lodash/get'
+import GoogleFontLoader from 'react-google-font-loader'
 import { openQuickViewAction } from '../../components/MainLayout/actions'
 import { compose } from 'react-apollo'
 import { connect } from 'react-redux'
 import queryString from 'query-string'
+import { getFonts } from './data'
 import ThreeD from '../../components/Render3D'
 import * as designsActions from './actions'
-import { QueryProps, DesignSaved } from '../../types/common'
+import { QueryProps, DesignSaved, Font } from '../../types/common'
 // TODO: Commented all quickview related until confirm it won't be needed
 // import quickView from '../../assets/quickview.svg'
 import {
@@ -29,6 +32,7 @@ interface Props extends RouteComponentProps<any> {
   data: Data
   // openQuickViewAction: (index: number) => void
   loadingModel: boolean
+  fontsData: any
   // Redux actions
   setLoadingAction: (loading: boolean) => void
 }
@@ -49,18 +53,30 @@ export class Designs extends React.Component<Props, {}> {
   // }
 
   render() {
-    const { location } = this.props
+    const { location, fontsData } = this.props
 
     const { search } = location
     const queryParams = queryString.parse(search)
     const designId = queryParams.id || ''
-
+    const fontList: Font[] = get(fontsData, 'fonts', [])
+    const installedFonts = fontList.reduce<{ font: string }[]>(
+      (fontObject, { active, family }) => {
+        if (active) {
+          fontObject.push({ font: family })
+        }
+        return fontObject
+      },
+      []
+    )
     return (
       <Container>
         {/* <Row>
           <Model>{name}</Model>
           <QuickView onClick={this.handleOpenQuickView} src={quickView} />
         </Row>*/}
+        {installedFonts.length ? (
+          <GoogleFontLoader fonts={installedFonts} />
+        ) : null}
         <ThreeD {...{ designId }} />
       </Container>
     )
@@ -71,6 +87,7 @@ const mapStateToProps = (state: any) => state.get('designs').toJS()
 
 const DesignsEnhance = compose(
   injectIntl,
+  getFonts,
   connect(
     mapStateToProps,
     { ...designsActions, openQuickViewAction }
