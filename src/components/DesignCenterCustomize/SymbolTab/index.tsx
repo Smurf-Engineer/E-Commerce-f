ae61308f022546ac24bcff386d505179103317cb
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2019-06-06 19:02:46 -0600

    Max connections pool

--- src/components/DesignCenterCustomize/SymbolTab/index.tsx
+++ src/components/DesignCenterCustomize/SymbolTab/index.tsx
@@ -61,7 +61,7 @@ interface Props {
 class SymbolTab extends React.PureComponent<Props, {}> {
   searchrAfterTyping = debounce(
     param => this.props.setSearchClipParamAction(param),
-    300
+    600
   )
   state = {
     option: 0,
