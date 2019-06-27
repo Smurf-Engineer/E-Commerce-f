19a8a68656593d65ded4ca4a262751ef65d71291
Carlos Cazarez <carlos@tailrecursive.co>
2019-06-26 20:18:14 -0600

    added unit tests for collection and gender filters in product catalogue

--- src/screens/ProductCatalogue/test.tsx
+++ src/screens/ProductCatalogue/test.tsx
@@ -3,7 +3,7 @@
  */
 
 import productCatalogReducer, { initialState } from './reducer'
-import { defaultAction } from './actions'
+import { defaultAction, setSelectedFilters } from './actions'
 
 describe(' ProductCatalog Screen', () => {
   // Test redux actions
@@ -14,7 +14,7 @@ describe(' ProductCatalog Screen', () => {
 
   it('Update someKey correctly', () => {
     const testValue = 'Test value'
-    const state = productCatalogReducer(initialState, defaultAction(testValue))
+    let state = productCatalogReducer(initialState, defaultAction(testValue))
     const someKey = state.get('someKey')
     expect(someKey).toEqual(testValue)
 
@@ -25,5 +25,191 @@ describe(' ProductCatalog Screen', () => {
     )
     const someKey2 = state2.get('someKey')
     expect(someKey2).toEqual(testValue2)
+  })
+
+  describe('Test SET_SELECTED_FILTERS action', () => {
+    it('Handle undefined initial value in collectionFilters', () => {
+      const customInitialValue = initialState.getIn([
+        'collectionFilters',
+        'Custom'
+      ])
+      expect(customInitialValue).toBeUndefined()
+
+      const inlineInitialValue = initialState.getIn([
+        'collectionFilters',
+        'Inline'
+      ])
+      expect(inlineInitialValue).toBeUndefined()
+    })
+
+    it('Handles true value in Custom and Inline filters', () => {
+      const collectionFiltersCustom = {
+        type: 'collectionFilters',
+        name: 'Custom'
+      }
+      const customFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(collectionFiltersCustom)
+      )
+      const customFilterValue = customFilterState.getIn([
+        collectionFiltersCustom.type,
+        collectionFiltersCustom.name
+      ])
+      expect(customFilterValue).toBeTruthy()
+
+      const collectionFiltersInline = {
+        type: 'collectionFilters',
+        name: 'Inline'
+      }
+      const inlineFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(collectionFiltersInline)
+      )
+      const inlineFilterValue = inlineFilterState.getIn([
+        collectionFiltersInline.type,
+        collectionFiltersInline.name
+      ])
+      expect(inlineFilterValue).toBeTruthy()
+    })
+
+    it('Handles false value in Custom and Inline filters', () => {
+      initialState.setIn(['collectionFilters', 'Inline'], true)
+      const collectionFiltersCustom = {
+        type: 'collectionFilters',
+        name: 'Custom'
+      }
+      const customFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(collectionFiltersCustom)
+      )
+      const customFilterValue = customFilterState.getIn([
+        collectionFiltersCustom.type,
+        collectionFiltersCustom.name
+      ])
+      expect(!customFilterValue).toBeFalsy()
+
+      initialState.setIn(['collectionFilters', 'Inline'], true)
+      const collectionFiltersInline = {
+        type: 'collectionFilters',
+        name: 'Inline'
+      }
+      const inlineFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(collectionFiltersInline)
+      )
+      const inlineFilterValue = inlineFilterState.getIn([
+        collectionFiltersInline.type,
+        collectionFiltersInline.name
+      ])
+      expect(!inlineFilterValue).toBeFalsy()
+    })
+
+    it('Handles undefined initial value in gender filters', () => {
+      const menFilterInitialValue = initialState.getIn(['genderFilters', 'Men'])
+      expect(menFilterInitialValue).toBeUndefined()
+
+      const womenFilterInitialValue = initialState.getIn([
+        'genderFilters',
+        'Women'
+      ])
+      expect(womenFilterInitialValue).toBeUndefined()
+
+      const unisexFilterInitialValue = initialState.getIn([
+        'genderFilters',
+        'Unisex'
+      ])
+      expect(unisexFilterInitialValue).toBeUndefined()
+    })
+
+    it('Handles true value in gender filters (Men, Women, Unisex)', () => {
+      const genderFiltersMen = {
+        type: 'genderFilters',
+        name: 'Men'
+      }
+      const menFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersMen)
+      )
+      const menFilterValue = menFilterState.getIn([
+        genderFiltersMen.type,
+        genderFiltersMen.name
+      ])
+      expect(menFilterValue).toBeTruthy()
+
+      const genderFiltersWomen = {
+        type: 'genderFilters',
+        name: 'Women'
+      }
+      const womenFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersWomen)
+      )
+      const womenFilterValue = womenFilterState.getIn([
+        genderFiltersWomen.type,
+        genderFiltersWomen.name
+      ])
+      expect(womenFilterValue).toBeTruthy()
+
+      const genderFiltersUnisex = {
+        type: 'genderFilters',
+        name: 'Unisex'
+      }
+      const unisexFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersUnisex)
+      )
+      const unisexFilterValue = unisexFilterState.getIn([
+        genderFiltersUnisex.type,
+        genderFiltersUnisex.name
+      ])
+      expect(unisexFilterValue).toBeTruthy()
+    })
+
+    it('Handles false value in gender filters (Men, Women, Unisex)', () => {
+      initialState.setIn(['genderFilters', 'Men'], true)
+      const genderFiltersMen = {
+        type: 'genderFilters',
+        name: 'Men'
+      }
+      const menFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersMen)
+      )
+      const menFilterValue = menFilterState.getIn([
+        genderFiltersMen.type,
+        genderFiltersMen.name
+      ])
+      expect(!menFilterValue).toBeFalsy()
+
+      initialState.setIn(['genderFilters', 'Women'], true)
+      const genderFiltersWomen = {
+        type: 'genderFilters',
+        name: 'Women'
+      }
+      const womenFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersWomen)
+      )
+      const womenFilterValue = womenFilterState.getIn([
+        genderFiltersWomen.type,
+        genderFiltersWomen.name
+      ])
+      expect(!womenFilterValue).toBeFalsy()
+
+      initialState.setIn(['genderFilters', 'Unisex'], true)
+      const genderFiltersUnisex = {
+        type: 'genderFilters',
+        name: 'Unisex'
+      }
+      const unisexFilterState = productCatalogReducer(
+        initialState,
+        setSelectedFilters(genderFiltersUnisex)
+      )
+      const unisexFilterValue = unisexFilterState.getIn([
+        genderFiltersUnisex.type,
+        genderFiltersUnisex.name
+      ])
+      expect(!unisexFilterValue).toBeFalsy()
+    })
   })
 })
