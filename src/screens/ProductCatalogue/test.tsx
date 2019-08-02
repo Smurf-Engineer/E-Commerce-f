214ff107b0d4e158fef4e9b4eddd8e84c3dafde5
Carlos Cazarez <carlos@tailrecursive.co>
2019-08-01 18:02:15 -0600

    Added final unit tests for product catalogue reducer

--- src/screens/ProductCatalogue/test.tsx
+++ src/screens/ProductCatalogue/test.tsx
@@ -3,7 +3,15 @@
  */
 
 import productCatalogReducer, { initialState } from './reducer'
-import { defaultAction, sortBySelected } from './actions'
+import {
+  defaultAction,
+  sortBySelected,
+  setSkipValue,
+  openSidebarMobile,
+  resetReducerAction,
+  clearFiltersAction,
+  setHomeSelectedFilters
+} from './actions'
 
 describe(' ProductCatalog Screen', () => {
   // Test redux actions
@@ -66,6 +74,121 @@ describe(' ProductCatalog Screen', () => {
       )
       const orderByValue = orderByState.get('orderBy')
       expect(orderByValue).toEqual(orderBy)
+    })
+  })
+
+  describe('SET_SKIP_VALUE', () => {
+    it('Handles initial value for skip', () => {
+      const skip = initialState.get('skip')
+      expect(skip).toEqual(0)
+    })
+
+    it('Handles initial value for currentPage', () => {
+      const currentPage = initialState.get('currentPage')
+      expect(currentPage).toEqual(1)
+    })
+
+    it('Handles update skip value correctly', () => {
+      const skipTest = 10
+      const pageTest = 2
+      const state = productCatalogReducer(
+        initialState,
+        setSkipValue(skipTest, pageTest)
+      )
+      const updatedSkip = state.get('skip')
+      expect(updatedSkip).toEqual(skipTest)
+    })
+
+    it('Handles update currentPage value correctly', () => {
+      const skipTest = 10
+      const pageTest = 2
+      const state = productCatalogReducer(
+        initialState,
+        setSkipValue(skipTest, pageTest)
+      )
+      const updatedCurrentPage = state.get('currentPage')
+      expect(updatedCurrentPage).toEqual(pageTest)
+    })
+  })
+
+  describe('OPEN_SIDEBAR_MOBILE', () => {
+    it('Should update openSidebar with true value correctly', () => {
+      const openSideBarTest = true
+      const state = productCatalogReducer(
+        initialState,
+        openSidebarMobile(openSideBarTest)
+      )
+      const openSideBarUpdated = state.get('openSidebar')
+      expect(openSideBarUpdated).toEqual(openSideBarTest)
+    })
+
+    it('Should update openSideBar with false value correctly', () => {
+      const openSideBarTest = false
+      const state = productCatalogReducer(
+        initialState,
+        openSidebarMobile(openSideBarTest)
+      )
+      const openSideBarUpdated = state.get('openSidebar')
+      expect(openSideBarUpdated).toBeFalsy()
+    })
+  })
+
+  describe('RESET_REDUCER_DATA', () => {
+    it('Should return inital state', () => {
+      const state = productCatalogReducer(initialState, resetReducerAction())
+      expect(state).toEqual(initialState)
+    })
+  })
+
+  describe('CLEAR_FILTERS', () => {
+    it('Should reset genderFilters', () => {
+      const genderFiltersInitial = initialState.get('genderFilters')
+      const state = productCatalogReducer(initialState, clearFiltersAction())
+      const genderFilters = state.get('genderFilters')
+      expect(genderFilters).toEqual(genderFiltersInitial)
+    })
+
+    it('Should reset sportFilters', () => {
+      const sportFiltersInitial = initialState.get('sportFilters')
+      const state = productCatalogReducer(initialState, clearFiltersAction())
+      const sportFilters = state.get('sportFilters')
+      expect(sportFilters).toEqual(sportFiltersInitial)
+    })
+
+    it('Should reset categoryFilters', () => {
+      const collectionFiltersInitial = initialState.get('collectionFilters')
+      const state = productCatalogReducer(initialState, clearFiltersAction())
+      const collectionFilters = state.get('collectionFilters')
+      expect(collectionFilters).toEqual(collectionFiltersInitial)
+    })
+  })
+
+  describe('SET_SELECTED_HOME_FILTERS', () => {
+    it('Should set roadBike filter to true', () => {
+      const state = productCatalogReducer(
+        initialState,
+        setHomeSelectedFilters()
+      )
+      const roadBike = state.getIn(['sportFilters', 'Road Bike'])
+      expect(roadBike).toBeTruthy()
+    })
+
+    it('Should set Jersey & Tops filter to true', () => {
+      const state = productCatalogReducer(
+        initialState,
+        setHomeSelectedFilters()
+      )
+      const jerseyAndTops = state.getIn(['categoryFilters', 'Jerseys & Tops'])
+      expect(jerseyAndTops).toBeTruthy()
+    })
+
+    it('Should set Inline filter to true', () => {
+      const state = productCatalogReducer(
+        initialState,
+        setHomeSelectedFilters()
+      )
+      const Inline = state.getIn(['collectionFilters', 'Inline'])
+      expect(Inline).toBeTruthy()
     })
   })
 })
