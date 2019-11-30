2080669f97ea76fa279dbb9bcdcba89f88731af9
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2019-11-29 17:29:07 -0700

    Removed update discount

--- src/components/DiscountsAdmin/data.ts
+++ src/components/DiscountsAdmin/data.ts
@@ -1,10 +1,10 @@
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
-export const addDiscountMutation = graphql(
+export const upsertDiscountMutation = graphql(
   gql`
-    mutation createDiscount($discount: DiscountInput!) {
-      discount: createDiscount(discount: $discount) {
+    mutation upsertDiscount($discount: DiscountInput!) {
+      discount: upsertDiscount(discount: $discount) {
         id
         code: coupon_code
         discountItemId: discount_item_id
@@ -26,34 +26,7 @@ export const addDiscountMutation = graphql(
     }
   `,
   {
-    name: 'addNewDiscount'
-  }
-)
-export const updateDiscountMutation = graphql(
-  gql`
-    mutation updateDiscount($discount: DiscountInput!) {
-      updateDiscount(discount: $discount) {
-        id
-        code: coupon_code
-        discountItemId: discount_item_id
-        type: discount_type
-        rate
-        expiry
-        active
-        restrictionType: restriction_type
-        usageNumber: usage_limit
-        selectedUsers: users {
-          netsuiteId: netsuite_internal
-          name: full_name
-          email
-          value: short_id
-        }
-        selectedProducts: items
-      }
-    }
-  `,
-  {
-    name: 'updateDiscount'
+    name: 'upsertDiscount'
   }
 )
 
