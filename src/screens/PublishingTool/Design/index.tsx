732d18815ff469d1c5b6f764822ebb5e5b1897c2
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2019-12-30 12:02:54 -0700

    Deleted colorButton component

--- src/screens/PublishingTool/Design/index.tsx
+++ src/screens/PublishingTool/Design/index.tsx
@@ -9,7 +9,6 @@ import Divider from 'antd/lib/divider'
 import { compose, withApollo } from 'react-apollo'
 import Palette from '../../../components/DesignPalette'
 import SwipeableViews from 'react-swipeable-views'
-import EditInspiration from '../EditInspiration'
 import { Container, Header, Title, Content, Button } from './styledComponents'
 import {
   Message,
@@ -22,7 +21,7 @@ import {
 const LIST_TAB = 0
 const EDIT_TAB = 1
 
-import { DESIGN_THUMBNAIL, DESIGN_COLORS, NONE } from '../reducer'
+import { DESIGN_THUMBNAIL, DESIGN_COLORS } from '../reducer'
 
 interface Props {
   productData: ProductData
@@ -72,31 +71,15 @@ export class Design extends React.Component<Props, {}> {
   render() {
     const {
       formatMessage,
-      colorBlock,
       colorIdeas,
       design,
       onEditColorIdea,
-      colorIdeaItem,
-      colorsList,
-      onSelectColorBlock,
-      onSelectColor,
-      onHoverColorBlock,
-      colorBlockHovered,
-      onUpdateColorIdeaName,
       uploadingThumbnail,
-      colors,
       onAddColorIdea,
-      onSaveThumbnail,
       onDeleteInspiration
     } = this.props
     const { name, image, colors: designColors } = design
-    let colorIdea: DesignObject | ModelDesign | null = null
     let renderList = true
-    if (colorIdeaItem !== NONE) {
-      colorIdea =
-        colorIdeaItem === DESIGN_COLORS ? design : colorIdeas[colorIdeaItem]
-      renderList = false
-    }
 
     const colorIdeasList = colorIdeas.map(
       ({ id, name: ideaName, colors: ideaColors, image: thumbnail }, key) => (
@@ -146,23 +129,6 @@ export class Design extends React.Component<Props, {}> {
               <Divider>{formatMessage(messages.colorCombosList)}</Divider>
               {colorIdeasList}
             </div>
-            <EditInspiration
-              render={!renderList}
-              {...{
-                colors,
-                colorIdea,
-                colorBlock,
-                onSelectColor,
-                onEditColorIdea,
-                onSaveThumbnail,
-                colorsList,
-                colorBlockHovered,
-                onHoverColorBlock,
-                onSelectColorBlock,
-                onUpdateColorIdeaName,
-                formatMessage
-              }}
-            />
           </SwipeableViews>
         </Content>
       </Container>
