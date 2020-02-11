26942d7b520bf535aca8644bb021d26789edc1a4
Jesús Apodaca <jesus@tailrecursive.co>
2020-02-11 12:05:49 -0700

    Changed order

--- src/components/DesignCenterCustomize/Render3D/index.js
+++ src/components/DesignCenterCustomize/Render3D/index.js
@@ -226,10 +226,6 @@ class Render3D extends PureComponent {
       this.render3DModel(newProduct)
     }
     if (openSlaask && proAssistId) {
-      if (typeof window.Intercom === 'function') {
-        window.Intercom('hide')
-        window.Intercom('update', { hide_default_launcher: true })
-      }
       initSlaask({
         id: proAssistId,
         userId: loggedUserId,
@@ -353,9 +349,6 @@ class Render3D extends PureComponent {
       }
       this.container.removeChild(this.renderer.domElement)
       this.clearScene()
-    }
-    if (typeof window.Intercom === 'function') {
-      window.Intercom('update', { hide_default_launcher: false })
     }
     closeSlaask()
   }
