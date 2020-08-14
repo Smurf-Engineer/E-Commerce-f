019eeebbecac26ff6b5e9aff2925ec93e5d2c47a
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2020-08-13 20:50:00 -0600

    Discounts by design refactor

--- src/utils/utilsCheckout.ts
+++ src/utils/utilsCheckout.ts
@@ -30,34 +30,24 @@ export const getTaxesAndDiscount = (
   if (couponCode) {
     const { type, rate, restrictionType, products = [] } = couponCode
     const restrictions = restrictionType ? restrictionType.split(',') : []
+    console.log(restrictions)
     switch (type) {
       case PERCENTAGE_PROMO: // '%'
         if (!restrictions.includes(DESIGN)) {
-          if (taxesAmount && applySpecialTaxes) {
-            taxVatTotal = taxesAmount / 100
-            const totalNet = subtotal / (1 + taxVatTotal)
-            // for Austria and Germany we calculate discount
-            // discount = (totalNet + proDesignReview) * percentageDiscount
-            discount = (totalNet + proDesignFee) * (Number(rate) / 100)
-          } else {
-            // calculate discount with (subtotal + proDesignFee) * percentageDiscount
-            discount = (subtotal + proDesignFee) * (Number(rate) / 100)
-          }
+          // calculate discount with (subtotal + proDesignFee) * percentageDiscount
+          discount = (subtotal + proDesignFee) * (Number(rate) / 100)
         } else {
-          discount = products.reduce((totalDiscount: number, design) => {
-            const itemForDiscount = find(
-              productsPrices,
-              (productObject) => productObject.designId === design
-            )
-            if (itemForDiscount) {
-              return (
-                totalDiscount +
-                itemForDiscount.price *
-                  (Number(rate) / 100) *
-                  itemForDiscount.quantity
-              )
+          console.log(productsPrices)
+          discount = productsPrices.reduce((totalDiscount: number, design) => {
+            if (!products.includes(design.designId)) {
+              return totalDiscount
             }
-            return totalDiscount
+            const { price, quantity} = design
+
+            return (
+              totalDiscount +
+              price * (Number(rate) / 100) * quantity
+            )
             // tslint:disable-next-line: align
           }, 0)
         }
@@ -85,7 +75,7 @@ export const getTaxesAndDiscount = (
   }
 
   discount = roundDecimals(discount) // round to 2 decimals
-
+  console.log(discount)
   // taxes
   let taxGst = 0
   let taxPst = 0
