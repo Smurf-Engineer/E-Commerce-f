b11268c3083dc3beeeb2148d6ea8148196d5b57e
Jesús Apodaca <jesus@tailrecursive.co>
2020-10-06 16:41:02 -0600

    Added hide featured photos

--- src/screens/CustomProductDetail/index.tsx
+++ src/screens/CustomProductDetail/index.tsx
@@ -549,6 +549,7 @@ export class CustomProductDetail extends React.Component<Props, {}> {
               formatMessage,
               currentCurrency
             }}
+            hideFeatured={true}
           />
         </Container>
       </Layout>
