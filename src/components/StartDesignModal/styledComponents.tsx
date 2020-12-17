95c36456268f1d59e199f51b3bb4816df0cc4d42
Eduardo Quintero <eduardo@Eduardos-MacBook-Pro.local>
2020-12-17 10:39:30 -0700

    Fold content prefix

--- src/components/StartDesignModal/styledComponents.tsx
+++ src/components/StartDesignModal/styledComponents.tsx
@@ -103,53 +103,71 @@ interface FoldAnimationProps {
 }
 
 export const FoldContent  = styled.div`
-display: flex;
-justify-content: space-between;
-color: ${WHITE};
-width: 100%;
-transform-style: preserve-3d;
-animation-fill-mode: both;
--webkit-transform-origin: bottom;
-transform-origin: top;
-transition: visibility 0.3s, transform 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000), opacity 0.4s, max-height 0.4s;
-
-&.folded {
-  opacity: 0;
-  visibility: hidden;
-  -webkit-transform: rotateX(120deg);
-  transform: rotateX(120deg) scaleY(0);
-  max-height: 0;
-  transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
-  &::after {
-    visibility: hidden;
-    background: rgba(200, 55, 55, 1);
-    transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
-  }
-}
-&.unfolded {
-  max-height: 130px;
-  opacity: 1;
-  visibility: visible;
-  -webkit-transform: rotateX(0);
-  transform: rotateX(0);
-  transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
-  &::after {
-    visibility: visible;
-    background: rgba(200, 55, 55, 0);
-    transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
-  }
-}
-&::after {
-  position: absolute;
-  content: '';
-  display: block;
-  top: 0;
-  left: 0;
-  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  color: ${WHITE};
   width: 100%;
-  mix-blend-mode: normal;
-  transition: all 0.4s ease;
-}
+  animation-fill-mode: both;
+
+  transform-style: preserve-3d;
+  -webkit-transform-style: preserve-3d;
+
+  transform-origin: bottom
+  -webkit-transform-origin: bottom;
+
+  transform-origin: top
+  -webkit-transform-origin: top;
+
+  transition: visibility 0.3s, transform 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000), 
+    opacity 0.4s, max-height 0.4s;
+  -webkit-transition: visibility 0.3s, transform 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000), 
+    opacity 0.4s, max-height 0.4s;
+
+  &.folded {
+    opacity: 0;
+    visibility: hidden;
+    transform: rotateX(120deg) scaleY(0);
+    -webkit-transform: rotateX(120deg) scaleY(0);
+
+    max-height: 0;
+    transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
+    -webkit-transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
+    &::after {
+      visibility: hidden;
+      background: rgba(200, 55, 55, 1);
+      transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
+      -webkit-transition-delay: ${({ order }: FoldAnimationProps) => order === 5 ? 0 : 0.3 / order}s;
+    }
+  }
+  &.unfolded {
+    max-height: 130px;
+    opacity: 1;
+    visibility: visible;
+
+    transform: rotateX(0);
+    -webkit-transform: rotateX(0);
+
+    transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
+    -webkit-transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
+
+    &::after {
+      visibility: visible;
+      background: rgba(200, 55, 55, 0);
+      transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
+      -webkit-transition-delay: ${({ order }: FoldAnimationProps) => order === 1 ? 0 : order * 0.3}s;
+    }
+  }
+  &::after {
+    position: absolute;
+    content: '';
+    display: block;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    mix-blend-mode: normal;
+    transition: all 0.4s ease;
+  }
 `
 
 interface BannerProps {
