efc9c17fb30eb1f568eb4bf37970e50c639999ba
Jesus Apodaca <jesus@tailrecursive.co>
2021-05-07 12:06:41 -0600

    Added new tooltip message

--- src/components/DesignCenterCustomize/ColorButtons/styledComponents.tsx
+++ src/components/DesignCenterCustomize/ColorButtons/styledComponents.tsx
@@ -23,18 +23,8 @@ export const ShuffleButton = styled.div`
 `
 
 export const Icon = styled.img`
-  max-width: 24px;
+  width: 24px;
   object-fit: contain;
-  width: 100%;
   opacity: 50%;
   padding-top: 8px;
-  @media (max-width: 1360px) {
-    align-self: center;
-    max-width: 20px;
-    padding-top: 10px;
-  }
-  @media (min-width: 768px) and (max-width: 991px) {
-    max-width: 18px;
-    padding-top: 15px;
-  }
 `
