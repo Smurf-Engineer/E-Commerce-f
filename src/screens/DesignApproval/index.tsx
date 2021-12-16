a89cdf61d19b6b11906d0ce1ff111c237780637f
jessricardo <jesus@tailrecursive.co>
2021-12-16 15:50:21 -0700

    Fixed condition

--- src/screens/DesignApproval/index.tsx
+++ src/screens/DesignApproval/index.tsx
@@ -2300,7 +2300,7 @@ export class DesignApproval extends React.Component<Props, StateProps> {
                   </StyledTooltipMobile>
                 }
               </MobileRequestButtons>
-              {!!itemStatus && false &&
+              {!!itemStatus &&
                 <RenderSection>
                   {(readyToShow || designToApply) && designId && showRenderWindow &&
                     <Render3D
