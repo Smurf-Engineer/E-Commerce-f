19855634b236047262adeae4f0e0a0ccb5f902bd
jessricardo <jesus@tailrecursive.co>
2022-01-18 19:36:57 -0700

    Commit fix

--- src/screens/DesignApproval/index.tsx
+++ src/screens/DesignApproval/index.tsx
@@ -2449,7 +2449,7 @@ export class DesignApproval extends React.Component<Props, StateProps> {
                   }
                 </MobileRequestButtons>
               }
-              {!!itemStatus && false &&
+              {!!itemStatus &&
                 <RenderSection>
                   {(readyToShow || designToApply) && designId && showRenderWindow &&
                     <Render3D
