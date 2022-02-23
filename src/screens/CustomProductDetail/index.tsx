c5460fedb9e45dd9a73b43f79ae4606fd7e1e409
jessricardo <jesus@tailrecursive.co>
2022-02-22 19:36:51 -0700

    Commit fix

--- src/screens/CustomProductDetail/index.tsx
+++ src/screens/CustomProductDetail/index.tsx
@@ -868,7 +868,7 @@ export class CustomProductDetail extends React.Component<Props, {}> {
   gotToEditDesign = (designId: string) => () => {
     const { history, data } = this.props
     const productId = get(data, 'design.product.id', -1)
-    if (productId === -1) {
+    if (productId === 262) {
       warning({
         title: <strong>EDITING NOT AVAILABLE</strong>,
         width: 494,
