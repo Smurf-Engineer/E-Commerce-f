48694cd625996f9c16f66823cb3b48b9aba354d7
jessricardo <jesus@tailrecursive.co>
2022-03-10 13:20:05 -0700

    Fixed cross origin

--- public/js/loaders/OBJLoader.js
+++ public/js/loaders/OBJLoader.js
@@ -331,7 +331,7 @@ THREE.OBJLoader = (function () {
 			var scope = this;
 
 			var loader = new THREE.FileLoader(scope.manager);
-			loader.setCrossOrigin('anonymous');
+			loader.crossOrigin = 'anonymous';
 			loader.setPath(this.path);
 			loader.load(url, function (text) {
 
