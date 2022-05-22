a83290a54c26801686946673b800627805075203
jessricardo <jesus@tailrecursive.co>
2022-05-22 00:56:34 -0600

    Added fedex tracking history

---
+++ src/components/TrackingInfo/constants.ts
@@ -0,0 +1,17 @@
+export const statusColors = {
+    'PU': '#25ae61',
+    'IT': '#48aaea',
+    'DL': 'green'
+}
+
+export const statusIcons = {
+    'PU': 'inbox',
+    'IT': 'info-circle',
+    'DL': 'check-circle'
+}
+
+export const statusSteps = {
+    'PU': 1,
+    'IT': 2,
+    'DL': 3
+}
